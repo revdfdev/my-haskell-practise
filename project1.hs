19da9e8385792ee806af92872620ed7d9ef13a66
Rehan Kodekar <mkodekar@zoho.com>
2018-04-20 01:02:51 +0530

    My haskell practise

---
+++ project1.hs
@@ -0,0 +1,20 @@
+import Data.List
+import System.IO
+
+-- Int -- -2^63 2^
+
+-- Integer -- 
+
+
+maxInt = maxBound :: Int
+minInt = minBound :: Int
+
+-- Floats --
+thisISADouble = 3.00000009991991 + 1.00002010010101
+-- Doubles --
+
+
+-- True And False for Bools--
+
+-- Const value --
+
